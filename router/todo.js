e13eb166900e172397bc3e5bc4cffd22fa170edc
devdmitchell <dmitchell.prsvr@gmail.com>
2025-01-08 08:32:07 -0600

    completed

--- router/todo.js
+++ router/todo.js
@@ -20,5 +20,36 @@ router.get(`/get-all-todos`, (req, res)=>{
 })
 
 
+router.get(`/get-todo-by-id/:id`, (req, res)=>{
+  const { id } = req.params
+  const todoItem = todos.find(todo => todo.id === id)
+
+  if (!todoItem) {
+      return res.json({message:'The Todo ID you are looking for does not exist, please check the ID.'})
+  }
+  res.json({message: "Todo found", payload: todoItem })
+})
+
+
+router.get('/get-todos-by-done/:done', (req, res) => {
+  const { done } = req.params
+  const isDone = done === 'true'
+  const isNotDone = done === 'false'
+  const newDoneArray = todos.filter(todo => todo.done === done)
+  res.json({message:`Todos with done status: ${done}`, payload: newDoneArray})
+})
+
+
+router.post('/create-new-todo', (req, res) => {
+  const { todo } = req.body
+  const newTodo = {
+    id: uuidv4(),
+    todo: todo,
+    done: "false"
+}
+todos.push(newTodo)
+res.json({payload: todos})
+})
+
 
 module.exports = router
